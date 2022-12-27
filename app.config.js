02318d833ca26d569ae234666df8c4a4ccbc6baa
Julien Usson <usson.julien@gmail.com>
2022-12-27 12:24:53 +0100

    version from package.json

---
+++ app.config.js
@@ -0,0 +1,37 @@
+const pkg = require('./package.json')
+
+module.exports = {
+  expo: {
+    name: "Bow'tir",
+    slug: pkg.name,
+    version: pkg.version,
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+      package: 'com.julienusson.arcount',
+      versionCode: pkg.versionCode,
+    },
+    extra: {
+      eas: {
+        projectId: 'ccd86831-63ff-46d2-bbe9-f85d10f1a4fa',
+      },
+    },
+  },
+}
--- src/screens/More.tsx
+++ src/screens/More.tsx
@@ -2,7 +2,9 @@ import { useNavigation } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Text, View } from 'react-native'
 
+import pkg from '../../package.json'
 import { List, ListItemButton } from '../components/List'
 import { RootStackParamList } from '../Main'
 
@@ -13,13 +15,16 @@ export default function MoreScreen() {
   const navigation = useNavigation<Navigation>()
 
   return (
-    <List>
-      <ListItemButton onPress={() => navigation.navigate('SightAdjustments')} icon="tune">
-        {t('Sight Adjustments')}
-      </ListItemButton>
-      <ListItemButton onPress={() => navigation.navigate('PrivacyPolicy')} icon="privacy-tip">
-        {t('Privacy Policy')}
-      </ListItemButton>
-    </List>
+    <View className="flex-1">
+      <List>
+        <ListItemButton onPress={() => navigation.navigate('SightAdjustments')} icon="tune">
+          {t('Sight Adjustments')}
+        </ListItemButton>
+        <ListItemButton onPress={() => navigation.navigate('PrivacyPolicy')} icon="privacy-tip">
+          {t('Privacy Policy')}
+        </ListItemButton>
+      </List>
+      <Text className="text-center font-ligth text-gray-300">v{pkg.version}</Text>
+    </View>
   )
 }
